7938d2bf3e1eee2faaaeeed248cefdb470ceb1bc
Gustavo Grando Vieira <97984848+gugrando@users.noreply.github.com>
2023-02-14 20:15:36 -0300

    Add files via upload

---
+++ js/favorites.js
@@ -0,0 +1,74 @@
+export class Favorites{
+  constructor(root){
+    this.tbody = this.root.querySelector('table tbody')
+    this.root = document.querySelector(root)
+    this.load()
+  }
+
+  
+  load(){
+    const entries = JSON.parse(localStorage.getItem("@github-favorites:")) || []
+  }
+
+  delete(user){
+    const filteredEntries = this.entries.filter(entry => entry.login !== entry.login)
+    this.entries = filteredEntries
+    this.update()
+  }
+}
+
+export class FavoritesView extends Favorites{
+  constructor(root){
+    super(root)
+    this.update()
+  }
+
+  update(){
+   this.removeAllTr()
+   this.entries.forEach(user =>{
+    const row = this.createRow()
+    
+    row.querySelector(".user img").src = `https://github.com/${user.login}.png`
+    row.querySelector(".user img").alt = `Imagem de ${user.name}`
+    row.querySelector(".user p").textContent = user.name
+    row.querySelector(".user span").textContent = user.login
+    row.querySelector(".repositories").textContent = user.public_repos
+    row.querySelector(".followers").textContent = user.followers
+    row.querySelector(".remove").onclick = () =>{
+      const isOK = confirm('Tem certeza que deseja deletar essa linha?')
+      if(isOK){
+        user.delete()
+      }
+    }
+
+    
+    this.tbody.append(row)
+  })
+  }
+
+  createRow(){
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+    <td class="user">
+      <img src="https://avatars.githubusercontent.com/u/97984848?s=400&u=49beaad904f72a7e0f35f3e2c6e0306c6452a9a2&v=4" alt="">
+      <a href="https://github.com/gugrando">
+        <p>gugrando</p>
+        <span>Gu grnado</span>
+      </a>
+    </td>
+    <td class="repositories"></td>
+    <td class="followers"></td>
+    <td>
+      <button class="remove">&times;</button>
+    </td>
+    `
+    return tr
+  }
+
+  removeAllTr(){  
+    this.tbody.querySelectorAll('tr')
+    .forEach((tr) => {
+      tr.remove()
+    });
+  }
+}
